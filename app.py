f6a9a93e64fd3c6093f760437b84ce88d52592c4
alexcastro02522-tech <alexcastro02522@gmail.com>
2026-09-22 15:56:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from reportlab.pdfgen import canvas
 import streamlit as st
 import psycopg2
 
-# LIGAÇÃO DIRETA BLINDADA AO SUPABASE (Nuvem)
+# LIGAÇÃO DIRETA BLINDADA AO SUPABASE COM SSL OBRIGATÓRIO
 SUPABASE_DB_URL = "postgresql://postgres:bz8VNak7mmgXO05i@db.ctibigorhywnwkuzqfjm.supabase.co:5432/postgres"
 
 # CONFIGURAÇÃO DO MERCADO PAGO
